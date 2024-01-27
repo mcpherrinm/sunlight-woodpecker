e0e033d6ab06bd68ef3ba9b1fa9407ef6920cf2e
Matthew McPherrin <git@mcpherrin.ca>
2024-01-27 15:53:34 -0500

    Extract the sunlight indexes from SCTs
    Functions pulled from sunlight, but should be library calls in the future.

--- internal/sunclient/ct.go
+++ internal/sunclient/ct.go
@@ -2,12 +2,15 @@ package sunclient
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
 	ct "github.com/google/certificate-transparency-go"
 	"github.com/google/certificate-transparency-go/client"
 	"github.com/google/certificate-transparency-go/jsonclient"
+	"golang.org/x/crypto/cryptobyte"
 )
 
 type Log struct {
@@ -24,16 +27,67 @@ func New(url string) (*Log, error) {
 
 func (l *Log) SubmitPreCert(ctx context.Context, asn1cert []byte) (*ct.SignedCertificateTimestamp, error) {
 	sct, err := l.ct.AddPreChain(ctx, []ct.ASN1Cert{{Data: asn1cert}})
+	if err != nil {
+		return nil, err
+	}
 
-	// TODO: This is where we load and verify the sunlight tiles
+	if err = l.SunlightValidate(ctx, asn1cert, sct); err != nil {
+		return nil, err
+	}
 
-	return sct, err
+	return sct, nil
 }
 
 func (l *Log) SubmitFinal(ctx context.Context, asn1cert []byte) error {
-	_, err := l.ct.AddChain(ctx, []ct.ASN1Cert{{Data: asn1cert}})
+	sct, err := l.ct.AddChain(ctx, []ct.ASN1Cert{{Data: asn1cert}})
+	if err != nil {
+		return err
+	}
 
-	// TODO: This is where we load and verify the sunlight tiles
+	return l.SunlightValidate(ctx, asn1cert, sct)
+}
 
-	return err
+// SunlightValidate takes a certificate and SCT to check its inclusion in the log.
+//
+// The leaf index is extracted from the SCT. We load tiles along with the STH to validate inclusion.
+func (l *Log) SunlightValidate(ctx context.Context, asn1cert []byte, sct *ct.SignedCertificateTimestamp) error {
+	_, err := sunlightIndex(sct.Extensions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// sunlightIndex parses the extensions from an SCT, finds the leaf_index extension, and returns the value
+// non-public function taken from sunlight
+func sunlightIndex(extensions ct.CTExtensions) (uint64, error) {
+	b := cryptobyte.String(extensions)
+	for !b.Empty() {
+		var extensionType uint8
+		var extension cryptobyte.String
+		if !b.ReadUint8(&extensionType) || !b.ReadUint16LengthPrefixed(&extension) {
+			return 0, errors.New("invalid extension")
+		}
+		if extensionType == 0 /* leaf_index */ {
+			var leafIndex int64
+			if !readUint40(&extension, &leafIndex) || !extension.Empty() {
+				return 0, errors.New("invalid leaf_index extension")
+			}
+			return 0, fmt.Errorf("index %d", leafIndex)
+		}
+	}
+	return 0, errors.New("missing leaf_index extension")
+}
+
+// readUint40 decodes a big-endian, 40-bit value into out and advances over it.
+// It reports whether the read was successful.
+// non-public function taken from sunlight
+func readUint40(s *cryptobyte.String, out *int64) bool {
+	var v []byte
+	if !s.ReadBytes(&v, 5) {
+		return false
+	}
+	*out = int64(v[0])<<32 | int64(v[1])<<24 | int64(v[2])<<16 | int64(v[3])<<8 | int64(v[4])
+	return true
 }
